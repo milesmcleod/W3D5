c4eb84f9b6640902b7533a4c9f53d3c79a74be80
MilesMcLeod <milesmcleod123@gmail.com>
2017-10-20 13:46:07 -0700

    Complete ARL phase IIIa

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -10,23 +10,51 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    self.class_name.downcase + "s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    if options[:class_name].nil?
+      @class_name = name.camelcase
+    else
+      @class_name = options[:class_name]
+    end
+    if options[:primary_key].nil?
+      @primary_key = :id
+    else
+      @primary_key = options[:primary_key]
+    end
+    if options[:foreign_key].nil?
+      @foreign_key = "#{@class_name.downcase}_id".to_sym
+    else
+      @foreign_key = options[:foreign_key]
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    if options[:class_name].nil?
+      @class_name = name.camelcase.singularize
+    else
+      @class_name = options[:class_name].singularize
+    end
+    if options[:primary_key].nil?
+      @primary_key = :id
+    else
+      @primary_key = options[:primary_key]
+    end
+    if options[:foreign_key].nil?
+      @foreign_key = "#{self_class_name.downcase}_id".to_sym
+    else
+      @foreign_key = options[:foreign_key]
+    end
   end
 end
 
