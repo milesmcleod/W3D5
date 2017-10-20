df596ab552f9c32a41ea940b074f0e1950aebe6a
MilesMcLeod <milesmcleod123@gmail.com>
2017-10-20 12:50:09 -0700

    Complete phase 2 searchable

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -1,9 +1,11 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
+
   def self.columns
     return @columns if @columns
     values = DBConnection.execute2(<<-SQL)
